d94f82ae8b3482ef54e33b7ff7cbbc5be3669265
xuzewei <xuzewei_2012@126.com>
2018-01-18 15:35:46 +0800

    logout

--- html/assets/scripts/fn.js
+++ html/assets/scripts/fn.js
@@ -10,6 +10,10 @@ function fn_api(data, success) {
 	});
 }
 
+function fn_remove_token() {
+	fn_set_token(null);
+}
+
 function fn_set_token(token) {
 	$.cookie("token", token, { "expires": 30, "path": "/" });
 }
