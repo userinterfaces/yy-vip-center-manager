1f456531400826215422246e2ada8733be908c2d
xuzewei <xuzewei_2012@126.com>
2018-01-18 16:49:43 +0800

    修改密码

--- html/assets/scripts/fn.js
+++ html/assets/scripts/fn.js
@@ -1,4 +1,4 @@
-function fn_api(data, success) {
+function fn_public_api(data, success) {
 	$.post("http://47.104.17.187:8085/yy-vip-center-manager-api", JSON.stringify(data), function(resp){
 		if (resp.code == 0) {
 			if (typeof success == 'function') {
@@ -8,6 +8,15 @@ function fn_api(data, success) {
 			toastr.warning(resp.message);
 		}
 	});
+}
+
+function fn_api(data, success) {
+	if (fn_get_token() == null) {
+		toastr.error("Token已失效，请重新登录");
+		return;
+	}
+	data.token = fn_get_token();
+	fn_public_api(data, success);
 }
 
 function fn_remove_token() {
