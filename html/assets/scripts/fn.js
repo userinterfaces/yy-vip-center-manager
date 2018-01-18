a2659235bbd1b0a5c0011512a55297afab9430f8
xuzewei <xuzewei_2012@126.com>
2018-01-18 15:19:46 +0800

    fn.js

--- html/assets/scripts/fn.js
+++ html/assets/scripts/fn.js
@@ -1,7 +1,9 @@
 function fn_api(data, success) {
 	$.post("http://47.104.17.187:8085/yy-vip-center-manager-api", JSON.stringify(data), function(resp){
 		if (resp.code == 0) {
-			success(resp);
+			if (typeof success == 'function') {
+				success(resp);
+			}
 		} else {
 			toastr.warning(resp.message);
 		}
@@ -9,30 +11,9 @@ function fn_api(data, success) {
 }
 
 function fn_set_token(token) {
-	$.cookie("token", token, { expires: 30, path: '/' });
+	$.cookie("token", token, { "expires": 30, "path": "/" });
 }
 
 function fn_get_token() {
-	$.cookie("token");
-}
-
-function set_cookie(c_name, value, expiredays) {
-	var exdate = new Date();
-	exdate.setDate(exdate.getDate()+expiredays);
-	document.cookie = c_name + "=" + escape(value) + ((expiredays==null) ? "" : ";expires=" + exdate.toGMTString());
-}
-
-function get_cookie(c_name) {
-	if (document.cookie.length > 0) {
-		var c_start = document.cookie.indexOf(c_name + "=");
-		if (c_start != -1) {
-			c_start = c_start + c_name.length + 1;
-			c_end = document.cookie.indexOf(";", c_start);
-			if (c_end == -1) {
-				c_end = document.cookie.length;
-			}
-			return unescape(document.cookie.substring(c_start,c_end));
-		}
-	}
-	return "";
+	return $.cookie("token");
 }
